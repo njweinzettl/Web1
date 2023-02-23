6e550661b1dbb73687dbe604e8e91e8c1e410217
Nadja Weinzettl (1000119812) <WEINNA1@student.op.ac.nz>
2023-02-24 11:34:39 +1300

    https://github.com/njweinzettl/Web1.git

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
+<!-- CSS stzling -->
+
+<style>
+    h1{
+        color: blue;
+        font-size: 34px;
+        font-style: italic;
+        font-weight: normal;
+    }
+</style>
+
+<!-- Javascript style -->
+<script>
+    const num = 43;
+    var name = "Adon";
+    let first_name = "Adon";
+
+    let count = 0;
+
+    function incrementCount()
+    {
+        count++ ;
+    }
+</script>
+
+<!-- html -->
+<button on:click={incrementCount}>
+    Clicked {count} {count === 1? "time" : "times"}
+</button>
+
+<!-- line 28 is same as if(count === 1) return time else return times-->
+
+<!-- make comments -->
+
+<h1>Welcome to SvelteKit {first_name} </h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
